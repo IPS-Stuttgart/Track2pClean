7e3503497659f5fa7cc45baabbf43c1c282d7385
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-05-18 08:02:52 +0200

    Fix FOV-affine test slice lint

--- tests/test_fov_affine_registration.py
+++ tests/test_fov_affine_registration.py
@@ -15,7 +15,9 @@ from bayescatrack.track2p_registration import register_plane_pair
 def _spot_image(shape: tuple[int, int], centers: tuple[tuple[int, int], ...]) -> np.ndarray:
     image = np.zeros(shape, dtype=float)
     for y, x in centers:
-        image[y - 1:y + 2, x - 1:x + 2] = 1.0
+        y_start, y_stop = y - 1, y + 2
+        x_start, x_stop = x - 1, x + 2
+        image[y_start:y_stop, x_start:x_stop] = 1.0
     return image
 
 
