26e78575a7328c8d5f11d66e5a2fc92b50f32c5c
Florian Pfaff <pfaff@ias.uni-stuttgart.de>
2026-06-04 16:16:04 +0200

    Update documented PyRecEst pin

--- src/bayescatrack/dependency_pins.py
+++ src/bayescatrack/dependency_pins.py
@@ -3,5 +3,5 @@
 from __future__ import annotations
 
 PYRECEST_REPOSITORY = "https://github.com/FlorianPfaff/PyRecEst.git"
-PYRECEST_COMMIT = "8df52f9283360b14cfee670b89e486bec8c1140c"
+PYRECEST_COMMIT = "6ad4680a187f13b88e2cd6e4485e16c7f115962a"
 PYRECEST_DIRECT_URL = f"git+{PYRECEST_REPOSITORY}@{PYRECEST_COMMIT}"
