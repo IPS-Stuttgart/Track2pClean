4f9d2702eea14ed407eff5f184987bd159e7a956
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-06-24 13:01:31 +0200

    Install dynamic edge prior bool validation

--- src/bayescatrack/association/__init__.py
+++ src/bayescatrack/association/__init__.py
@@ -6,13 +6,16 @@ from . import (
     _calibrated_mahalanobis_bundle_patch as _calibrated_mahalanobis_bundle_patch,
 )
 from . import _calibrated_roi_stat_feature_patch as _calibrated_roi_stat_feature_patch
+from . import _dynamic_edge_prior_validation as _dynamic_edge_prior_validation
 from . import _global_assignment_input_validation as _global_assignment_input_validation
 
 _global_assignment_input_validation.install_global_assignment_input_validation()
+_dynamic_edge_prior_validation.install_dynamic_edge_prior_bool_validation()
 
 _PATCH_MODULES = (
     _calibrated_mahalanobis_bundle_patch,
     _calibrated_roi_stat_feature_patch,
+    _dynamic_edge_prior_validation,
     _global_assignment_input_validation,
 )
 
