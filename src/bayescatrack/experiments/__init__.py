f9eab40c089d58f5b8bbd9ea95754f12dc2a943d
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-06-25 22:06:37 +0200

    Use direct manifest integration

--- src/bayescatrack/experiments/__init__.py
+++ src/bayescatrack/experiments/__init__.py
@@ -5,7 +5,6 @@ from . import _cost_sweep_defaults as _cost_sweep_defaults
 from . import _diag_defaults as _diag_defaults
 from . import _seed_session_validation as _seed_session_validation
 from . import _teacher_rescue_manifest_integration as _teacher_rescue_manifest_integration
-from . import _teacher_rescue_manifest_reload_fix as _teacher_rescue_manifest_reload_fix
 from . import (
     _triplet_support_benchmark_integration as _triplet_support_benchmark_integration,
 )
@@ -29,7 +28,6 @@ _triplet_support_benchmark_integration.install_track2p_benchmark_triplet_support
 _seed_session_validation.install_seed_session_validation()
 install_benchmark_roi_index_validation()
 install_calibration_feature_registry_integration()
-_teacher_rescue_manifest_reload_fix.install_teacher_rescue_manifest_reload_fix()
 _teacher_rescue_manifest_integration.install_teacher_rescue_manifest_integration()
 install_teacher_rescue_edit_cap_manifest_integration()
 install_teacher_rescue_repair_preset_manifest_integration()
