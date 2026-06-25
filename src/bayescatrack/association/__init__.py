742f83e395e47f5170be14400037772959c1b951
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-06-25 03:38:22 +0200

    Install neuropil ratio shape validation patch

--- src/bayescatrack/association/__init__.py
+++ src/bayescatrack/association/__init__.py
@@ -9,6 +9,7 @@ from . import (
 from . import _calibrated_roi_stat_feature_patch as _calibrated_roi_stat_feature_patch
 from . import _dynamic_edge_prior_validation as _dynamic_edge_prior_validation
 from . import _global_assignment_input_validation as _global_assignment_input_validation
+from . import _neuropil_ratio_shape_validation as _neuropil_ratio_shape_validation
 from . import _roi_aware_local_validation as _roi_aware_local_validation
 from . import _session_edge_pair_validation as _session_edge_pair_validation
 from . import (
@@ -16,6 +17,7 @@ from . import (
 )
 
 _activity_similarity_control_validation.install_activity_similarity_control_validation()
+_neuropil_ratio_shape_validation.install_neuropil_ratio_shape_validation()
 _session_edge_pair_validation.install_session_edge_pair_validation()
 _global_assignment_input_validation.install_global_assignment_input_validation()
 _roi_aware_local_validation.install_roi_aware_local_validation()
@@ -28,6 +30,7 @@ _PATCH_MODULES = (
     _calibrated_roi_stat_feature_patch,
     _dynamic_edge_prior_validation,
     _global_assignment_input_validation,
+    _neuropil_ratio_shape_validation,
     _roi_aware_local_validation,
     _session_edge_pair_validation,
     _track2p_policy_session_gap_validation,
