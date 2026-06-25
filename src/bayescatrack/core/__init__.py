8cb0f1bda1cd5160c552b367f9793e884bb1bb40
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-06-25 16:30:29 +0200

    Validate subject export boolean options

--- src/bayescatrack/core/__init__.py
+++ src/bayescatrack/core/__init__.py
@@ -1,6 +1,11 @@
 """Core BayesCaTrack exports."""
 
 from .._exports import reexport
+from . import _bridge_impl as _bridge_impl
+from . import _subject_export_option_validation as _subject_export_option_validation
+
+_subject_export_option_validation.install_subject_export_option_validation(_bridge_impl)
+
 from . import bridge as _bridge
 
 __all__ = reexport(_bridge, globals())
