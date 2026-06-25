026cd65ff298b9668145791adab7bbeeb2c1c6e0
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-06-25 22:00:21 +0200

    Mark track2pclean namespace as typed (#763)
    * Mark track2pclean namespace as typed
    
    * Add py.typed marker for track2pclean
    
    * Test track2pclean typed marker

--- tests/test_track2pclean_cli.py
+++ tests/test_track2pclean_cli.py
@@ -1,3 +1,5 @@
+import importlib.resources
+
 from tests._support import run_module
 
 
@@ -27,3 +29,9 @@ def test_track2pclean_benchmark_subcommand_help_uses_native_program_name():
     proc = run_module("-m", "track2pclean", "benchmark", "track2p", "--help")
 
     assert "usage: track2pclean benchmark track2p" in proc.stdout
+
+
+def test_track2pclean_is_marked_as_typed_package():
+    marker = importlib.resources.files("track2pclean") / "py.typed"
+
+    assert marker.is_file()
