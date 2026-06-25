ea5be2d54ff6335d2f25b7fcaf5f10b42af2a3c8
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-06-25 04:13:14 +0200

    Install numpy boolean loader validation patch

--- src/bayescatrack/core/bridge.py
+++ src/bayescatrack/core/bridge.py
@@ -5,6 +5,7 @@
 from .._exports import BRIDGE_PUBLIC_NAMES
 from . import _bridge_impl
 from . import _cell_probability_validation as _cell_probability_validation
+from . import _loader_bool_validation as _loader_bool_validation
 from . import _loader_validation as _loader_validation
 from . import _local_evidence as _local_evidence
 from . import _mahalanobis as _mahalanobis
@@ -12,6 +13,7 @@ from . import _roi_index_validation as _roi_index_validation
 from . import _roi_stat_features as _roi_stat_features
 
 _cell_probability_validation.install_cell_probability_cost_patch(_bridge_impl)
+_loader_bool_validation.install_numpy_bool_loader_validation()
 _loader_validation.install_loader_validation_patches(_bridge_impl)
 _roi_index_validation.install_calcium_plane_roi_index_validation(
     _bridge_impl.CalciumPlaneData
