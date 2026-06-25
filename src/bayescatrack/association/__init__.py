875c46778ecb69d593e538416e5fb2a64df4c8c2
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-06-25 04:26:26 +0200

    Validate shifted-IoU preset controls

--- src/bayescatrack/association/__init__.py
+++ src/bayescatrack/association/__init__.py
@@ -11,6 +11,7 @@ from . import _dynamic_edge_prior_validation as _dynamic_edge_prior_validation
 from . import _global_assignment_input_validation as _global_assignment_input_validation
 from . import _roi_aware_local_validation as _roi_aware_local_validation
 from . import _session_edge_pair_validation as _session_edge_pair_validation
+from . import _shifted_iou_preset_validation as _shifted_iou_preset_validation
 from . import (
     _track2p_policy_session_gap_validation as _track2p_policy_session_gap_validation,
 )
@@ -19,6 +20,7 @@ _activity_similarity_control_validation.install_activity_similarity_control_vali
 _session_edge_pair_validation.install_session_edge_pair_validation()
 _global_assignment_input_validation.install_global_assignment_input_validation()
 _roi_aware_local_validation.install_roi_aware_local_validation()
+_shifted_iou_preset_validation.install_shifted_iou_preset_validation()
 _dynamic_edge_prior_validation.install_dynamic_edge_prior_bool_validation()
 _track2p_policy_session_gap_validation.install_track2p_policy_session_gap_validation()
 
@@ -30,6 +32,7 @@ _PATCH_MODULES = (
     _global_assignment_input_validation,
     _roi_aware_local_validation,
     _session_edge_pair_validation,
+    _shifted_iou_preset_validation,
     _track2p_policy_session_gap_validation,
 )
 
