ac66a763cd5a0f36b41bf1d404c1bc729168fd91
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-06-25 02:14:30 +0200

    Install Track2p-policy session-gap validation

--- src/bayescatrack/association/__init__.py
+++ src/bayescatrack/association/__init__.py
@@ -10,11 +10,15 @@ from . import _dynamic_edge_prior_validation as _dynamic_edge_prior_validation
 from . import _global_assignment_input_validation as _global_assignment_input_validation
 from . import _roi_aware_local_validation as _roi_aware_local_validation
 from . import _session_edge_pair_validation as _session_edge_pair_validation
+from . import (
+    _track2p_policy_session_gap_validation as _track2p_policy_session_gap_validation,
+)
 
 _session_edge_pair_validation.install_session_edge_pair_validation()
 _global_assignment_input_validation.install_global_assignment_input_validation()
 _roi_aware_local_validation.install_roi_aware_local_validation()
 _dynamic_edge_prior_validation.install_dynamic_edge_prior_bool_validation()
+_track2p_policy_session_gap_validation.install_track2p_policy_session_gap_validation()
 
 _PATCH_MODULES = (
     _calibrated_mahalanobis_bundle_patch,
@@ -23,6 +27,7 @@ _PATCH_MODULES = (
     _global_assignment_input_validation,
     _roi_aware_local_validation,
     _session_edge_pair_validation,
+    _track2p_policy_session_gap_validation,
 )
 
 __all__ = reexport(_bridge, globals(), ASSOCIATION_PUBLIC_NAMES)
