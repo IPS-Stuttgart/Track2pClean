50dd59be0afd25307b6561a86c7a847a54f47a51
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-06-26 04:29:09 +0200

    Install subject export bool validation (#784)
    * Install export bool validation hook
    
    * Add export bool validation regression test

--- src/bayescatrack/core/bridge.py
+++ src/bayescatrack/core/bridge.py
@@ -5,6 +5,7 @@
 from .._exports import BRIDGE_PUBLIC_NAMES
 from . import _bridge_impl
 from . import _cell_probability_validation as _cell_probability_validation
+from . import _export_bool_validation as _export_bool_validation
 from . import _loader_bool_validation as _loader_bool_validation
 from . import _loader_validation as _loader_validation
 from . import _local_evidence as _local_evidence
@@ -17,6 +18,7 @@ from . import (
 from . import _suite2p_overlap_value_validation as _suite2p_overlap_value_validation
 
 _cell_probability_validation.install_cell_probability_cost_patch(_bridge_impl)
+_export_bool_validation.install_subject_export_bool_validation(_bridge_impl)
 _loader_bool_validation.install_numpy_bool_loader_validation()
 _loader_validation.install_loader_validation_patches(_bridge_impl)
 _suite2p_coordinate_value_validation.install_suite2p_coordinate_value_validation(
