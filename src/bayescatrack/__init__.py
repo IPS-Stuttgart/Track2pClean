a54d50369b5ee6fa9e208d091d1685d002c98729
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-06-26 02:10:11 +0200

    Install multisession config validation

--- src/bayescatrack/__init__.py
+++ src/bayescatrack/__init__.py
@@ -52,6 +52,9 @@ from ._matching_max_cost_validation import (
 from ._matching_validation import (
     install_matching_layout_validation as _install_matching_layout_validation,
 )
+from ._multisession_config_validation import (
+    install_multisession_config_validation as _install_multisession_config_validation,
+)
 from ._multisession_solver_track_validation import (
     install_multisession_solver_track_validation as _install_multisession_solver_track_validation,
 )
@@ -160,6 +163,7 @@ _strict_config_validation._positive_int = _candidate_prefilter._positive_int
 _install_empty_candidate_gate_margin_fix()
 _install_empty_registered_roi_mask_validation()
 _install_matching_bundle_roi_index_validation()
+_install_multisession_config_validation()
 _install_multisession_solver_track_validation()
 _install_session_gap_validation()
 _install_session_match_result_validation()
