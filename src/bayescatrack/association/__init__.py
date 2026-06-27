78415f6eefe833c01555a11b054c4fc730039260
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-06-27 11:02:43 +0200

    Install absence config scalar validation

--- src/bayescatrack/association/__init__.py
+++ src/bayescatrack/association/__init__.py
@@ -2,6 +2,7 @@
 
 from .._exports import ASSOCIATION_PUBLIC_NAMES, reexport
 from ..core import bridge as _bridge
+from . import _absence_config_scalar_validation as _absence_config_scalar_validation
 from . import _absence_cue_shape_validation as _absence_cue_shape_validation
 from . import _activity_similarity_control_validation as _activity_similarity_control_validation
 from . import (
@@ -28,6 +29,7 @@ from . import _track_refinement_fill_value_validation as _track_refinement_fill_
 from . import _triplet_support_validation as _triplet_support_validation
 from . import absence_model as _absence_model
 
+_absence_config_scalar_validation.install_absence_config_scalar_validation(_absence_model)
 _absence_cue_shape_validation.install_absence_cue_shape_validation(_absence_model)
 _activity_similarity_control_validation.install_activity_similarity_control_validation()
 _neuropil_ratio_shape_validation.install_neuropil_ratio_shape_validation()
@@ -45,6 +47,7 @@ _track_refinement_fill_value_validation.install_track_refinement_fill_value_vali
 _growth_coordinate_validation.install_growth_coordinate_validation()
 
 _PATCH_MODULES = (
+    _absence_config_scalar_validation,
     _absence_cue_shape_validation,
     _activity_similarity_control_validation,
     _calibrated_mahalanobis_bundle_patch,
