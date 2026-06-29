1f716b759cce9e2388929071987d0dc965e91e1b
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-06-29 17:49:36 +0200

    Install loader numeric conversion validation

--- src/bayescatrack/core/__init__.py
+++ src/bayescatrack/core/__init__.py
@@ -2,8 +2,12 @@
 
 from .._exports import reexport
 from . import _export_bool_validation as _export_bool_validation
+from . import _loader_conversion_validation as _loader_conversion_validation
 from . import bridge as _bridge
 
+_loader_conversion_validation.install_loader_numeric_conversion_validation(
+    _bridge._loader_validation
+)  # pylint: disable=protected-access
 _export_bool_validation.install_subject_export_bool_validation(
     _bridge._bridge_impl
 )  # pylint: disable=protected-access
