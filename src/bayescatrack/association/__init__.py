1def8b9135f3b3b86711eb68d750f3bc823d1d88
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-07-01 17:22:48 +0200

    Install teacher prior ROI token validation

--- src/bayescatrack/association/__init__.py
+++ src/bayescatrack/association/__init__.py
@@ -40,6 +40,7 @@ from . import _roi_aware_local_validation as _roi_aware_local_validation
 from . import _session_edge_pair_validation as _session_edge_pair_validation
 from . import _shifted_iou_preset_validation as _shifted_iou_preset_validation
 from . import _teacher_prior_index_validation as _teacher_prior_index_validation
+from . import _teacher_prior_roi_token_validation as _teacher_prior_roi_token_validation
 from . import (
     _track2p_policy_session_gap_validation as _track2p_policy_session_gap_validation,
 )
@@ -82,6 +83,7 @@ _roi_aware_local_validation.install_roi_aware_local_validation()
 _registered_component_shape_validation.install_registered_component_shape_validation()
 _shifted_iou_preset_validation.install_shifted_iou_preset_validation()
 _teacher_prior_index_validation.install_teacher_prior_index_validation()
+_teacher_prior_roi_token_validation.install_teacher_prior_roi_token_validation()
 _dynamic_edge_prior_validation.install_dynamic_edge_prior_bool_validation()
 _dynamic_activity_component_validation.install_dynamic_activity_component_shape_validation()
 _track2p_policy_session_gap_validation.install_track2p_policy_session_gap_validation()
@@ -120,6 +122,7 @@ _PATCH_MODULES = (
     _session_edge_pair_validation,
     _shifted_iou_preset_validation,
     _teacher_prior_index_validation,
+    _teacher_prior_roi_token_validation,
     _track2p_policy_session_gap_validation,
     _track_refinement_binary_token_validation,
     _track_refinement_fill_value_validation,
